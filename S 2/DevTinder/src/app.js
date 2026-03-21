ea0a40340b34c9022b7ce023ac8bed5493d19229
Happy <pmgc590@gmail.com>
2026-03-21 17:28:38 +0530

    Route Handler Basics

--- S 2/DevTinder/src/app.js
+++ S 2/DevTinder/src/app.js
@@ -48,6 +48,119 @@ app.get("/abc", (req, res) => {
     res.send({ firstName: "James", lastName: "Roy" });
 });
 
+// // Empty Route
+// app.use("/user", (req, res) => {
+//     // Route Handler
+//     // res.send("Route Handler 1");  
+//     console.log("Handling the route user!!");             
+// })
+
+// ----------------------------------------------------------------------------
+
+// // One Route can also have Multiple Route Handler
+// app.use("/user", (req, res, next) => {
+//     // Route Handler
+//     console.log("Handling the route user!!");             
+//     // res.send("Route Handler 1");  
+//     next();
+// }, (req, res, next) => {
+//     // Route Handler 2
+//     console.log("Handling the route user 2!!");             
+//     // res.send("Route Handler 2");
+//     next();  
+// }, (req, res, next) => {
+//     // Route Handler 3
+//     console.log("Handling the route user 3!!");             
+//     // res.send("Route Handler 3");  
+//     next();
+// }, (req, res, next) => {
+//     // Route Handler 4
+//     console.log("Handling the route user 4!!");             
+//     // res.send("Route Handler 4");  
+//     next();
+// }, (req, res, next) => {
+//     // Route Handler 5
+//     console.log("Handling the route user 5!!");             
+//     // res.send("Route Handler 5");  
+//     next();
+// }, (req, res, next) => {
+//     // Route Handler 6
+//     console.log("Handling the route user 6!!");             
+//     res.send("Route Handler 6");  
+// });
+
+// ----------------------------------------------------------------------------
+
+// Route Handler can also be wrap inside []
+app.use("/user", [(req, res, next) => {
+    // Route Handler
+    console.log("Handling the route user!!");             
+    // res.send("Route Handler 1");  
+    next();
+}, (req, res, next) => {
+    // Route Handler 2
+    console.log("Handling the route user 2!!");             
+    // res.send("Route Handler 2");
+    next();  
+}, (req, res, next) => {
+    // Route Handler 3
+    console.log("Handling the route user 3!!");             
+    // res.send("Route Handler 3");  
+    next();
+}, (req, res, next) => {
+    // Route Handler 4
+    console.log("Handling the route user 4!!");             
+    // res.send("Route Handler 4");  
+    next();
+}, (req, res, next) => {
+    // Route Handler 5
+    console.log("Handling the route user 5!!");             
+    // res.send("Route Handler 5");  
+    next();
+}, (req, res, next) => {
+    // Route Handler 6
+    console.log("Handling the route user 6!!");             
+    res.send("Route Handler 6");  
+}]);
+
+// ----------------------------------------------------------------------------
+
+// Route Handler can also mix and match 
+// Suppose u want to wrap only R1, R2 inside [] (Array)
+app.use("/user", [(req, res, next) => {
+    // Route Handler
+    console.log("Handling the route user!!");             
+    // res.send("Route Handler 1");  
+    next();
+}, (req, res, next) => {
+    // Route Handler 2
+    console.log("Handling the route user 2!!");             
+    // res.send("Route Handler 2");
+    next();  
+}], (req, res, next) => {
+    // Route Handler 3
+    console.log("Handling the route user 3!!");             
+    // res.send("Route Handler 3");  
+    next();
+}, (req, res, next) => {
+    // Route Handler 4
+    console.log("Handling the route user 4!!");             
+    // res.send("Route Handler 4");  
+    next();
+}, (req, res, next) => {
+    // Route Handler 5
+    console.log("Handling the route user 5!!");             
+    // res.send("Route Handler 5");  
+    next();
+}, (req, res, next) => {
+    // Route Handler 6
+    console.log("Handling the route user 6!!");             
+    res.send("Route Handler 6");  
+});
+
+
+
+
 app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777...");
 });
